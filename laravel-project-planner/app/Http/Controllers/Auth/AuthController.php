f34d643a91cb756edaf34aad8f8dbb51cc8e9d35
ChanHeinKyaw <scm.chanheinkyaw@gmail.com>
2023-01-20 16:28:00 +0630

    add validation

--- laravel-project-planner/app/Http/Controllers/Auth/AuthController.php
+++ laravel-project-planner/app/Http/Controllers/Auth/AuthController.php
@@ -6,39 +6,54 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $user = User::where('email', $request->email)->first();
-        if ($user) {
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'max:255', 'min:6'],
+            'email' => ['required', 'unique:users,email'],
+            'password' => ['required', 'max:255', 'min:6'],
+        ]);
+
+        if ($validator->fails()) {
             return response()->json([
                 'success' => false,
                 'status' => 500,
-                'data' => 'Email is already register',
-            ]);
-        } else {
-            $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => bcrypt($request->password),
-            ]);
-
-            $token = $user->createToken('user')->accessToken;
-
-            return response()->json([
-                'success' => true,
-                'status' => 200,
-                'data' => ['token' => $token, 'user' => $user],
+                'data' => $validator->errors(),
             ]);
         }
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $token = $user->createToken('user')->accessToken;
+
+        return response()->json([
+            'success' => true,
+            'status' => 200,
+            'data' => ['token' => $token, 'user' => $user],
+        ]);
     }
 
     public function login(Request $request)
     {
-        Log::info($request->all());
+        $validator = Validator::make($request->all(), [
+            'email' => ['required'],
+            'password' => ['required', 'max:255', 'min:6'],
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'status' => 500,
+                'data' => $validator->errors(),
+            ]);
+        }
+
         $user = User::where('email', $request->email)->first();
         if ($user) {
             if (Hash::check($request->password, $user->password)) {
@@ -63,7 +78,5 @@ class AuthController extends Controller
                 'data' => 'wrong_email',
             ]);
         }
-
-        return $user;
     }
 }
